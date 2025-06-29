e77d4edd8ed4c9c9d065224580ae361e3b1d2085
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-06-29 22:01:20 +0700

    ubah view mahasiswa

--- app/view/mahasiswa/index.php
+++ app/view/mahasiswa/index.php
@@ -1,9 +1,9 @@
-<div class="container mt-5">
-    <div class="row">
+<div class="container mt-5 ">
+    <div class="row justify-content-center align-items-start">
 
-        <h3>Daftar Mahasiswa</h3>
+        <h1 class="text-center">Daftar Mahasiswa</h1>
         <?php foreach ($data['mhs'] as $mhs) : ?>
-            <div class="card mx-3" style="width: 18rem;">
+            <div class="card mx-3 my-3" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title"><?= $mhs->nama ?></h5>
                     <h6 class="card-subtitle mb-2 text-body-secondary" style="--bs-text-opacity: .9;">Jurusan <?= $mhs->jurusan ?></h6>
