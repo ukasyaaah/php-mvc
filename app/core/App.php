fc8bda03290f3e07a731bc98cf75d71458b6baee
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-06-29 14:17:09 +0700

    Routing

--- app/core/App.php
+++ app/core/App.php
@@ -4,6 +4,23 @@ class App
 {
     function __construct()
     {
-        echo "Oke";
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    function parseURL()
+    {
+        if (isset($_GET['url'])) {
+            // rtrim : bersihkan tanda slash diakhir url
+            $url = rtrim($_GET['url'], '/');
+
+            // Bersihkan url dari karakter2 aneh
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+
+            // Pisah url dengan / menggunakan explode
+            $url = explode('/', $url);
+
+            return $url;
+        }
     }
 }
